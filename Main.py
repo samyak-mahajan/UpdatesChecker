788e5af90d914756c71a5fe1efc27de2422f4a12
QuantElektroAstrophysicist <76886561+QuantElektroAstrophysicist@users.noreply.github.com>
2021-03-07 05:50:32 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,8 @@
+from selenium.webdriver import Edge
+from moodlechecker import *
+"""We are importing selenium modules functions for Edge .You can use your preferred browser ,just read the configuration file for detailed instructions"""
+PATH='C:/WebDriver/bin/msedgedriver.exe'
+##Edge(executable_path=PATH) make sure you give PATH as an argument!
+driver=Edge(PATH)
+
+moodle_login()
